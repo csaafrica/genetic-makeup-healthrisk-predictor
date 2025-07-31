e3c3d3b8aaa2965875b5e82dfa7b7340791801fb
Ryanjuguna <ryanjuguna2k2@gmail.com>
2025-07-31 13:03:19 +0300

    update main.py

--- main.py
+++ main.py
@@ -42,7 +42,8 @@ st.title("BMI Predictor App")
 st.markdown("Upload a CSV file with the required features to predict BMI.")
 
 st.markdown(f"Your table should have all the features: {', '.join(features_set)}, including SNPS from SNP_1 to SNP_1000.")
-st.markdown(f"---Here is a sample of the data used to train the model---\n, {bio_data.head().to_markdown(index=False)}")
+st.markdown(f"---Here is a sample of the data used to train the model---")
+st.table(bio_data.head())
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
